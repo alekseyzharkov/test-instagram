53383c6f9f285d54f9ebe24db8305c108610dbba
alekseyzharkov <57396563+alekseyzharkov@users.noreply.github.com>
2022-06-16 19:17:29 +0300

    second commit (#1)

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+
+use Phpfastcache\Helper\Psr16Adapter;
+
+require 'vendor/autoload.php';
+
+$instagram = \InstagramScraper\Instagram::withCredentials(new \GuzzleHttp\Client([
+    'proxy' => 'XXX',
+]), 'username', 'password', new Psr16Adapter('Files'));
+$instagram->login();
+sleep(2); // Delay to mimic user
+
+$username = 'extraversum';
+$followers = [];
+$account = $instagram->getAccount($username);
+sleep(1);
+$followers = $instagram->getFollowing($account->getId(), 1000, 100, true); // Get 1000 followings of 'kevin', 100 a time with random delay between requests
+echo '<pre>' . json_encode($followers, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . '</pre>';
